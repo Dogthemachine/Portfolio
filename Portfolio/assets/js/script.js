139d5f7c8f205cf3a62389abeca2ea07f78c2282
Dogthemachine <danileyko@gmail.com>
2022-09-22 17:49:32 +0200

    making video css

--- Portfolio/assets/js/script.js
+++ Portfolio/assets/js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
 
+//-----------    CHANGE VIDEO     -----------
+//    const horizontal_vid = document.getElementById('horizontal_vid');
+//    const vertical_vid = document.createElement('video');
+//    vertical_vid.innerHTML = '<video src="{{ STATIC_URL }}img/reel_v.mp4" id="vertical_vid" muted loop autoplay></video>';
+//
+//    if (window.innerHeight > window.innerWidth) {
+//        horizontal_vid.parentNode.replaceChild(vertical_vid, horizontal_vid);
+//    };
+//-----------   CHANGE VIDEO     -----------
+
 
 });
