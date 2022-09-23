7d7e22b55b60c90c1339c97c9f73300f6a0a3bc3
Dogthemachine <danileyko@gmail.com>
2022-09-23 10:05:49 +0200

    making vertical vid

--- Portfolio/assets/js/script.js
+++ Portfolio/assets/js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
 //-----------    CHANGE VIDEO     -----------
-//    const horizontal_vid = document.getElementById('horizontal_vid');
-//    const vertical_vid = document.createElement('video');
-//    vertical_vid.innerHTML = '<video src="{{ STATIC_URL }}img/reel_v.mp4" id="vertical_vid" muted loop autoplay></video>';
-//
-//    if (window.innerHeight > window.innerWidth) {
-//        horizontal_vid.parentNode.replaceChild(vertical_vid, horizontal_vid);
-//    };
+    const horizontal_vid = document.getElementById('horizontal_vid');
+    const vertical_vid = document.createElement('video');
+    vertical_vid.innerHTML = '<video playsinline autoplay muted loop poster="reel.jpg" id="vertical_vid"><source src="{{ STATIC_URL }}img/reel_v.webm" type="video/webm"><source src="{{ STATIC_URL }}img/reel_v.mp4" type="video/mp4"><source src="{{ STATIC_URL }}img/reel_v.ogv" type="video/ogv"></video>';
+
+    if (window.innerHeight > window.innerWidth) {
+        horizontal_vid.parentNode.replaceChild(vertical_vid, horizontal_vid);
+    };
 //-----------   CHANGE VIDEO     -----------
 
 
