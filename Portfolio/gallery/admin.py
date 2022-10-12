1e38c469b4f2a10c526cc0e6a9c981d68ba183ba
Dogthemachine <danileyko@gmail.com>
2022-10-12 18:25:23 +0200

    adding schema and open graph tags, also webm and posters to vid

--- Portfolio/gallery/admin.py
+++ Portfolio/gallery/admin.py
@@ -7,10 +7,10 @@ from gallery.models import Photos as Photo
 from django.contrib.auth.models import Group
 
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'photo')
+    list_display = ('id', 'name', 'photo')
 
 class VideoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'video')
+    list_display = ('id', 'name', 'video', 'video_webm', 'poster')
 
 admin.site.register(Photo, PhotoAdmin)
 
--- Portfolio/gallery/models.py
+++ Portfolio/gallery/models.py
@@ -4,15 +4,22 @@ from django.core.validators import FileExtensionValidator
 
 
 class Photos(models.Model):
+    name = models.CharField(max_length=200, blank=True)
     photo = ResizedImageField(size=[2500, 2500], upload_to="photos/%Y/%m/%d")
 
     def __str__(self):
-        return u"(%s)" % (self.id)
+        return u"(%s)" % (self.name)
 
 
 class Videos(models.Model):
+    name = models.CharField(max_length=200, blank=True)
     video = models.FileField(upload_to='videos_uploaded',null=True,
                              validators=[FileExtensionValidator(allowed_extensions=['MOV','avi','mp4','webm','mkv'])])
+    video_webm = models.FileField(upload_to='videos_uploaded',null=True,
+                             validators=[FileExtensionValidator(allowed_extensions=['MOV','avi','mp4','webm','mkv'])])
+    poster = ResizedImageField(size=[2500, 2500], upload_to="photos/%Y/%m/%d", blank=True)
+    duration = models.CharField(max_length=50, blank=True)
+
 
     def __str__(self):
-        return u"(%s)" % (self.id)
+        return u"(%s)" % (self.name)
